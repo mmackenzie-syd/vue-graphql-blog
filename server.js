9e9eaf5696c1429bd457a0089192739ab596dee6
mmackenzie-syd <mmackenzie.syd@gmail.com>
2021-01-10 07:29:33 +1100

    part 11-15

--- server.js
+++ server.js
@@ -1,33 +1,33 @@
-const { ApolloServer, gql } = require('apollo-server');
+const { ApolloServer } = require("apollo-server");
+const mongoose = require("mongoose");
+const fs = require("fs");
+const path = require("path");
+const resolvers = require("./resolvers");
 
-const mongoose = require('mongoose');
+const filePath = path.join(__dirname, "typeDefs.gql");
+const typeDefs = fs.readFileSync(filePath, "utf-8");
 
-require('dotenv').config({path: 'variables.env'})
+require("dotenv").config({ path: "variables.env" });
+const User = require("./models/User");
+const Post = require("./models/Post");
 
-console.log('path', process.env.MONGO_URI)
+mongoose
+    .connect(
+        process.env.MONGO_URI,
+        { useNewUrlParser: true }
+    )
+    .then(() => console.log("DB connected"))
+    .catch(err => console.error(err));
 
-mongoose.connect(process.env.MONGO_URI)
-    .then(()=> console.log('DB Connected'))
-    .catch(err => console.log('DB connection error: ', err));
-
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
-const typeDefs = gql`
-  type Todo {
-    task: String
-    completed: Boolean
-  }
-  type Query {
-    getTodos: [Todo]
-  }
-`;
-
-
-const server = new ApolloServer({ typeDefs });
-
-// The `listen` method launches a web server.
-server.listen().then(({ url }) => {
-    console.log(`Server ready at ${url}`);
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: {
+        User,
+        Post
+    }
 });
 
+server.listen().then(({ url }) => {
+    console.log(`Server listening on ${url}`);
+});
