59294dc7f2160f4e92c792e088d40445534758b1
mmackenzie-syd <mmackenzie.syd@gmail.com>
2021-01-12 01:49:47 +1100

    part 23

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,8 +1,39 @@
 <template>
-  <v-app>
-    <!-- Horizontal nav-bar -->
+  <v-app style="background: #e3e3ee">
+    <!-- Side Nav Bar -->
+    <v-navigation-drawer
+        app
+        temporary
+        fixed
+        v-model="sideNav"
+    >
+      <v-toolbar color="accent" dark flat>
+        <v-app-bar-nav-icon @click="toggleSideNav"></v-app-bar-nav-icon>
+        <router-link to="/" tag="span" style="cursor: pointer">
+          <h1 class="title pl-3">VueShare</h1>
+        </router-link>
+      </v-toolbar>
+      <v-divider></v-divider>
+      <v-list>
+        <v-list-item
+            ripple
+          v-for="item in sideNavItems"
+          :key="item.title"
+          :to="item.link"
+        >
+          <v-list-item-action>
+            <v-icon>{{item.icon}}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            {{item.title}}
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <!-- Horizontal Nav Bar -->
       <v-app-bar fixed color="primary" dark app>
-        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-app-bar-nav-icon @click="toggleSideNav"></v-app-bar-nav-icon>
         <!-- app title -->
         <v-toolbar-title class="hidden-xs-only">
           <router-link to="/" tag="span" style="cursor: pointer">
@@ -63,11 +94,22 @@ export default {
         { icon: 'mdi-lock-open', title: 'Sign In', link: '/signin' },
         { icon: 'mdi-pencil', title: 'Sign Up', link: '/signup' },
       ]
+    },
+    sideNavItems() {
+      return [
+        { icon: 'mdi-comment-text', title: 'Posts', link: '/posts' },
+        { icon: 'mdi-lock-open', title: 'Sign In', link: '/signin' },
+        { icon: 'mdi-pencil', title: 'Sign Up', link: '/signup' },
+      ]
     }
   },
-
+  methods: {
+    toggleSideNav() {
+      this.sideNav = !this.sideNav;
+    }
+  },
   data: () => ({
-    //
+    sideNav: false
   }),
 };
 </script>
