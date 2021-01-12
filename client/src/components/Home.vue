72e42f694164e5c410f8846d6dd7681147c56e61
mmackenzie-syd <mmackenzie.syd@gmail.com>
2021-01-12 19:47:22 +1100

    part 28

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,22 +1,15 @@
 <template>
-  <v-container>
-    <h1>Home</h1>
-    <ApolloQuery :query="getPostsQuery">
-      <template slot-scope="{ result: { loading, error, data }}">
-        <div v-if="loading">Loading...</div>
-        <div v-if="error">Error! {{error.message}}</div>
-        <ul v-for="post in data.getPosts" :key="post._id">
-          <li >
-            {{post.title}}
-            {{post.imageUrl}}
-            {{post.description}}
-          </li>
-          <li>{{post.likes}}</li>
-        </ul>
-      </template>
-    </ApolloQuery>
+  <v-container v-if="getPosts">
+    <v-flex xs12>
+      <v-carousel v-bind="{ 'cycle': true }" interval="3000">
+        <v-carousel-item v-for="post in getPosts" :key="post._id" :src="post.imageUrl">
+          <h1 id="carousel__title">{{post.title}}</h1>
+        </v-carousel-item>
+      </v-carousel>
+    </v-flex>
   </v-container>
 </template>
+
 
 <script>
 // @ is an alias to /src
@@ -27,20 +20,37 @@ export default {
   name: 'Home',
   components: {
   },
-  data() {
-    return {
-      getPostsQuery: gql`
+  apollo: {
+    getPosts: {
+      query: gql`
         query {
-            getPosts {
-                _id
-                title
-                imageUrl
-                description
-                likes
-            }
-        }`
+          getPosts {
+            _id
+            title
+            imageUrl
+            description
+            likes
+          }
+        }
+      `
     }
-  },
-
+  }
 }
 </script>
+
+<style>
+#carousel__title {
+  position: absolute;
+  background-color: rgba(0, 0, 0, 0.5);
+  color: white;
+  border-radius: 5px 5px 0 0;
+  padding: 0.5em;
+  margin: 0 auto;
+  bottom: 50px;
+  left: 0;
+  right: 0;
+  text-align: center;
+  font-size: 22px;
+}
+
+</style>
