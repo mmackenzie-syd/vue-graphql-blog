9ed06cc84b1330393611d96eafb8d589c27fdc7b
mmackenzie-syd <mmackenzie.syd@gmail.com>
2021-01-13 08:20:15 +1100

    part 31

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,7 +1,22 @@
 <template>
-  <v-container v-if="posts.length > 0">
+  <v-container >
+    <v-layout row>
+      <v-dialog v-model="loading" persistent fullscreen>
+        <v-container fill-height>
+          <v-layout row justify-center align-center>
+            <v-progress-circular
+                indeterminate
+                :size="70"
+                :width="7"
+                color="secondary">
+
+            </v-progress-circular>
+          </v-layout>
+        </v-container>
+      </v-dialog>
+    </v-layout>
     <v-flex xs12>
-      <v-carousel v-bind="{ 'cycle': true }" interval="3000">
+      <v-carousel v-if="!loading && posts.length > 0" v-bind="{ 'cycle': true }" interval="3000">
         <v-carousel-item v-for="post in posts" :key="post._id" :src="post.imageUrl">
           <h1 id="carousel__title">{{post.title}}</h1>
         </v-carousel-item>
@@ -16,6 +31,8 @@
 
 import { gql } from 'apollo-boost';
 
+import { mapGetters } from 'vuex';
+
 export default {
   name: 'Home',
 
@@ -25,9 +42,7 @@ export default {
     this.handleGetCarouselPosts();
   },
   computed: {
-    posts() {
-      return this.$store.getters.posts;
-    }
+    ...mapGetters(["loading", "posts"])
   },
   methods: {
     handleGetCarouselPosts() {
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -9,36 +9,42 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-      posts: []
+      posts: [],
+      loading: false
   },
   mutations: {
       setPosts: (state, payload) => {
           state.posts = payload;
+      },
+      setLoading: (state, payload) => {
+          state.loading = payload;
       }
   },
   actions: {
         getPosts: ({ commit }) => {
+            commit('setLoading', true)
             apolloClient.query({
-            query: gql`
-                query {
-                getPosts {
-                    _id
-                    title
-                    imageUrl
-                }
-            }
-            `
+                query: gql`
+                    query {
+                    getPosts {
+                        _id
+                        title
+                        imageUrl
+                    }
+                }`
             }).then(({ data }) => {
-                commit('setPosts', data.getPosts)
-                console.log(data.getPosts);
+                commit('setPosts', data.getPosts);
+                commit('setLoading', false);
             }).catch(err => {
                 console.error(err);
+                commit('setLoading', false);
             })
         }
   },
   modules: {
   },
   getters: {
-      posts: state => state.posts
+      posts: state => state.posts,
+      loading: state => state.loading,
   }
 })
