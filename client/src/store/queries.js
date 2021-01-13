47b3946892336de760d088afe51bf33b01ccf491
mmackenzie-syd <mmackenzie.syd@gmail.com>
2021-01-13 16:53:27 +1100

    part 37

--- client/src/store/queries.js
+++ client/src/store/queries.js
@@ -14,5 +14,19 @@ export const GET_POSTS = gql`
 /* User Queries */
 
 /* User Mutations */
+export const SIGNIN_USER = gql`
+    mutation($username: String!, $password: String!) {
+    signinUser(username: $username, password: $password) {
+        token
+    }
+}`;
+
+export const SIGNUP_USER = gql`
+    mutation($username: String!, $email: String!, $password: String!) {
+        signupUser(username: $username, email: $email, password: $password) {
+            token
+        }
+    }
+`;
 
 /* Post Mutations */
