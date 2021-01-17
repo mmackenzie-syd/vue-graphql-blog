af1f3bd5318f20c3fdf94f6396974b5efb69ce50
mmackenzie-syd <mmackenzie.syd@gmail.com>
2021-01-17 15:24:54 +1100

    add post form

--- client/src/components/Posts/AddPost.vue
+++ client/src/components/Posts/AddPost.vue
@@ -1,7 +1,57 @@
 <template>
-  <v-container>
+  <div>
     <h1>Add Post</h1>
-  </v-container>
+    <v-form
+        v-model="valid"
+        lazy-validation
+        ref="form"
+        @submit.prevent="handleAddPost"
+    >
+      <v-text-field
+          v-model="title"
+          :rules="titleRules"
+          label="Post Title"
+          type="text"
+          required
+      />
+
+      <v-text-field
+          v-model="imageUrl"
+          :rules="imageRules"
+          label="Image Url"
+          type="text"
+          required
+      />
+      <div>
+        <img :src="imageUrl" height="500px" />
+      </div>
+
+      <v-select
+          v-model="categories"
+          :items="['Art', 'Education', 'Travel', 'Photography', 'Technology']"
+          multiple
+          label="Categories"
+          :rules="categoriesRules"
+      />
+
+      <v-textarea
+          v-model="description"
+          :rules="descRules"
+          label="Description"
+          required
+      />
+
+      <v-btn
+          class="mr-4 mb-3"
+          color="accent"
+          :loading="loading"
+          :disabled="!valid || loading"
+          type="submit"
+      >
+        Submit
+      </v-btn>
+    </v-form>
+  </div>
 </template>
 
 <script>
@@ -10,5 +60,28 @@
 
 export default {
   name: 'AddPost',
+  data() {
+    return {
+      valid: true,
+      title: '',
+      imageUrl: '',
+      categories: [],
+      description: '',
+      titleRules: [
+        title => !!title || 'Title is required',
+        title => title.length < 20 || 'Title must have less than 20 characters'
+      ],
+      imageRules: [
+        image => !!image || 'Image is required'
+      ],
+      categoriesRules: [
+        categories => categories.length >= 1 || 'At least one category is required'
+      ],
+      descRules: [
+        desc => !!desc || 'Description is required',
+        desc => desc.length < 200 || 'Description must have less than 200 characters',
+      ],
+    }
+  }
 }
 </script>
